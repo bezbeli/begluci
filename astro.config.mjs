9aba387093269486aff89422993d14866c981ad6
bezbeli <bezbeli@gmail.com>
2024-10-10 10:07:19 +0200

    Refactor import paths in astro.config.mjs and add path module
    - Update import paths in astro.config.mjs to use the path module for resolving aliases.
    - Add path module import to astro.config.mjs.
    
    Refactor netlify.toml with updated environment variables
    
    - Update RESEND_API_KEY and SEND_EMAIL_FROM environment variables in netlify.toml.
    
    Update package.json and tsconfig.json
    
    - Update dependencies in package.json.
    - Update baseUrl and paths in tsconfig.json.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,23 @@
 import { defineConfig } from "astro/config";
 import netlify from "@astrojs/netlify";
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
+import path from 'path';
 
-// https://astro.build/config
 export default defineConfig({
   vite: {
     resolve: {
       alias: {
-        '@': '/src',
+        '@': path.resolve('./src'),
       },
     },
   },
   output: "hybrid",
   adapter: netlify(),
-  integrations: [tailwind({
-    nesting: true
-  }), react()]
+  integrations: [
+    tailwind({
+      nesting: true
+    }),
+    react()
+  ]
 });
