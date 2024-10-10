c155d0e62b491ce9999209484c336a84ba36aac6
bezbeli <bezbeli@gmail.com>
2024-10-10 02:56:51 +0200

    Refactor input field placeholder handling

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,13 @@ import { defineConfig } from "astro/config";
 import netlify from "@astrojs/netlify";
 import tailwind from "@astrojs/tailwind";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
   output: "hybrid",
   adapter: netlify(),
-  integrations: [tailwind({ nesting: true })],
+  integrations: [tailwind({
+    nesting: true
+  }), react()]
 });
---
+++ src/components/ui/CarouselWrapper.tsx
@@ -0,0 +1,32 @@
+import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from "@/components/ui/carousel";
+import Fade from "node_modules/embla-carousel-fade";
+import Autoplay from "node_modules/embla-carousel-autoplay";
+import { useState, useEffect } from "react";
+
+const InteractiveCarousel = () => {
+  const [images, setImages] = useState<string[]>([]);
+
+  useEffect(() => {
+    const imagePaths = Array.from({ length: 12 }, (_, i) => `/images/${i + 1}.jpg`);
+    setImages(imagePaths);
+  }, []);
+
+  return (
+    <Carousel
+      className="mb-12"
+      plugins={[Fade(), Autoplay({ delay: 3000, stopOnFocusIn: false, stopOnInteraction: false })]}
+    >
+      <CarouselContent>
+        {images.map((image, index) => (
+          <CarouselItem key={index}>
+            <img src={image} alt={`Carousel image ${index + 1}`} />
+          </CarouselItem>
+        ))}
+      </CarouselContent>
+      <CarouselPrevious />
+      <CarouselNext />
+    </Carousel>
+  );
+};
+
+export default InteractiveCarousel;
