2e6091919edfe39192d31f837c1229ad6a570bcb
bezbeli <bezbeli@gmail.com>
2024-10-11 12:38:40 +0200

    Refactor ContactForm.astro: Update input and label IDs

--- src/pages/actions/send-custom-email.ts
+++ src/pages/actions/send-custom-email.ts
@@ -9,7 +9,7 @@ export const POST: APIRoute = async ({ request, redirect }) => {
   const to = import.meta.env.SEND_EMAIL_TO;
   const name = formData.get("name") as string | null;
   const email = formData.get("email") as string | null;
-  const subject = `${name} šalje poruku sa Begluka`;
+  const subject = `🚀 ${name}: Begluci.ba`;
   const message = formData.get("message") as string | null;
   console.log({ to, email, subject, name, message });
 
